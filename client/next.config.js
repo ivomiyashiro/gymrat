d04e8b0d138fe54b56e50bab43e8f97d3de35ff9
Ivan Miyashiro <ivo.miyashiro1@gmail.com>
2023-05-28 23:16:49 -0300

    Add api base url dev and prod

--- client/next.config.js
+++ client/next.config.js
@@ -4,7 +4,9 @@ const nextConfig = {
     GOOGLE_CLIENT_ID: process.env.SHOPIFY_ACCESS_TOKEN,
     GOOGLE_CLIENT_SECRET: process.env.SHOPIFY_STORE_API_URL,
     GOOGLE_OAUTH_REDIRECT_URL: process.env.BASE_API_URL,
-    API_BASE_URL: process.env.API_BASE_URL
+    API_BASE_URL: process.env.NODE_ENV === 'development' 
+      ? process.env.API_BASE_URL_DEV 
+      : process.env.API_BASE_URL_PROD
   },
   images: {
     domains: ['cdn.shopify.com'],
