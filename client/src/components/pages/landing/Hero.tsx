12be68a429ba6686f91caea37615aaaca5b80ff4
Ivan Miyashiro <ivo.miyashiro1@gmail.com>
2023-05-30 11:55:45 -0300

    Fix layout for bigger screens

--- client/src/components/pages/landing/Hero.tsx
+++ client/src/components/pages/landing/Hero.tsx
@@ -6,8 +6,8 @@ import { FadeUpContainer } from '@/components/ui';
 
 export const Hero = () => {
   return (
-    <section className='relative h-[calc(100vh-102px)]'>
-      <FadeUpContainer className='absolute top-0 z-10 h-full flex justify-center flex-col p-4 lg:px-10'>
+    <section className='relative h-[calc(100vh-102px)] max-h-[55rem]'>
+      <FadeUpContainer className='absolute top-0 z-10 flex h-full justify-center flex-col p-4 lg:px-10 w-full max-w-[1640px] mx-auto left-[50%] right-[50%] translate-x-[-50%]'>
         <h1 className='block text-white font-bold leading-[1.15em]'>
           UNLEASH YOUR
           <br />
@@ -17,12 +17,12 @@ export const Hero = () => {
           Flex what you’ve built with physique-enhancing cuts,
           compressive fits and comfortable lifting essentials.
         </p>
-        <Link href='/products' className='bg-blue-600 text-white px-6 py-3 w-[165px] rounded flex justify-center items-center gap-2 font-semibold'>
+        <Link href='/products' className='bg-gray-500 text-white px-6 py-3 w-[165px] rounded flex justify-center items-center gap-2 font-semibold hover:bg-orange-600 transition'>
           SEE MORE
           <NavArrowRight width={ 16 } height={ 16 } />
         </Link>
       </FadeUpContainer>
-      <div className='relative top-0 w-full max-h-[770px] h-full z-0'>
+      <div className='relative top-0 w-full h-full z-0'>
         <Image
           src='/images/hero.jpg'
           alt='Gymrat hero image'
