f298c0067e5df4f918716b6db37ce930943d401f
Ivan Miyashiro <ivo.miyashiro1@gmail.com>
2023-06-19 22:07:52 -0300

    Add dynamic category

--- client/src/components/sections/product/Recommendations/index.tsx
+++ client/src/components/sections/product/Recommendations/index.tsx
@@ -1,14 +1,15 @@
 'use client';
 import Link from 'next/link';
+import { TCategory } from '@/interfaces';
 import { useProducts } from '@/hooks';
 import { ProductsCarrousel } from '@/components/ui';
 
-export const Recommendations = () => {
-  const { adaptedProducts } = useProducts({ limit: 4, filters: [{ gender: 'MEN' }] });
+export const Recommendations = ({ category }: { category: TCategory }) => {
+  const { adaptedProducts } = useProducts({ limit: 4, filters: [{ category }] });
 
   return (
-    <section className='px-4 py-20 md:py-32 lg:px-24 w-full max-w-[1640px] mx-auto'>
-      <h2 className='font-bold'>YOU MAY LIKE</h2>
+    <section className='px-4 py-20 lg:px-24 w-full max-w-[1640px] mx-auto'>
+      <h2 className='font-bold text-4xl'>YOU MAY LIKE</h2>
       <ProductsCarrousel products={ adaptedProducts } limit={ 4 } />
       <div className='flex w-full justify-end'>
         <Link href='/products' className='underline hover:text-blue-500 text-right mt-6'>
