91f897a185211d2647d8bb1dc69cdedda7269af7
Ivan Miyashiro <ivo.miyashiro1@gmail.com>
2023-06-30 08:09:32 -0300

    Add delay

--- client/src/components/ui/FadeUpContainer/index.tsx
+++ client/src/components/ui/FadeUpContainer/index.tsx
@@ -3,13 +3,13 @@ import { ReactNode, useEffect, useRef, useState, Children } from 'react';
 import { useIntersectionObserver } from '@/hooks';
 
 interface Props {
-  index: number;
+  index?: number;
   children: ReactNode;
   delayFactor?: number;
   className?: string;
 }
 
-export const FadeUpContainer = ({ index, children, className, delayFactor = 0 }: Props) => {
+export const FadeUpContainer = ({ index = 0, children, className, delayFactor = 0 }: Props) => {
   const [visible, setVisible] = useState(false);
   const ref = useRef<HTMLDivElement>(null);
   const entry = useIntersectionObserver(ref, {});
@@ -25,7 +25,7 @@ export const FadeUpContainer = ({ index, children, className, delayFactor = 0 }:
       {Children.map(children, () => (
         <div
           ref={ ref }
-          className={ `transition duration-500 ${ className } ${visible ? 'opacity-100 translate-y-none' : 'opacity-0 translate-y-[100px]'}` }
+          className={ `transition duration-500 delay-150 ${ className } ${visible ? 'opacity-100 translate-y-none' : 'opacity-0 translate-y-[100px]'}` }
           style={ {
             transitionDelay: `${delayFactor > 0 ? index * delayFactor + 'ms' : '0'}`,
           } }
