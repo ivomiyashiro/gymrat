34d43a6ef1351ce4652fbc6089518cef93ba087b
Ivan Miyashiro <ivo.miyashiro1@gmail.com>
2023-06-30 08:59:49 -0300

    Add no products found

--- client/src/components/sections/products/ProductsList/index.tsx
+++ client/src/components/sections/products/ProductsList/index.tsx
@@ -1,8 +1,6 @@
 import { useContext } from 'react';
-
 import { TVariant } from '@/interfaces';
 import { CatalogContext } from '@/context';
-
 import { ProductCard, ProductCardSkeleton } from '@/components/ui';
 
 export const ProductsList = () => {
@@ -14,19 +12,35 @@ export const ProductsList = () => {
         loadingProducts
           ? new Array(10).fill(null).map((_val, i) => <ProductCardSkeleton key={ i } /> ) 
           : (
-            adaptedProducts.map(({ variantsToPrint, ...restOfProducts }) => {
-              return (
-                variantsToPrint?.map((variant: TVariant) => {
+            adaptedProducts.length === 0
+              ? (
+                <div className='w-full mb-16 mt-4'>
+                  <h3 className='font-semibold text-2xl text-gray-500'>Oops! We couldn&apos;t find any products...</h3>
+                </div>
+              )
+              : (
+                adaptedProducts.map(({ variantsToPrint, ...restOfProducts }) => {
+                  if (adaptedProducts.length === 1 && variantsToPrint?.length === 0 ) {
+                    return (
+                      <div className='w-full mb-16 mt-4' key={ 0 }>
+                        <h3 className='font-semibold text-2xl text-gray-500'>Oops! We couldn&apos;t find any products...</h3>
+                      </div>
+                    );
+                  }
+
                   return (
-                    <ProductCard
-                      key={ variant._id }
-                      product={ restOfProducts }
-                      variant={ variant }
-                    />
+                    variantsToPrint?.map((variant: TVariant) => {
+                      return (
+                        <ProductCard
+                          key={ variant._id }
+                          product={ restOfProducts }
+                          variant={ variant }
+                        />
+                      );
+                    })
                   );
                 })
-              );
-            })
+              )
           )
       }
     </div>
