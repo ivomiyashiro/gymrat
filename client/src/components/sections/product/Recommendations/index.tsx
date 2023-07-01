41ff1a4d798aaf9534313d896953c668e0266ee9
Ivan Miyashiro <ivo.miyashiro1@gmail.com>
2023-07-01 12:27:16 -0300

    Add loading to recommendations props

--- client/src/components/sections/product/Recommendations/index.tsx
+++ client/src/components/sections/product/Recommendations/index.tsx
@@ -5,12 +5,12 @@ import { useProducts } from '@/hooks';
 import { ProductsCarrousel } from '@/components/ui';
 
 export const Recommendations = ({ category }: { category: TCategory }) => {
-  const { adaptedProducts } = useProducts({ limit: 4, filters: [{ category }] });
+  const { adaptedProducts, loading } = useProducts({ limit: 4, filters: [{ category }] });
 
   return (
     <section className='px-4 py-20 lg:px-24 w-full max-w-[1640px] mx-auto'>
       <h2 className='font-bold text-4xl'>YOU MAY LIKE</h2>
-      <ProductsCarrousel products={ adaptedProducts } limit={ 4 } />
+      <ProductsCarrousel products={ adaptedProducts } limit={ 4 } loading={ loading }  />
       <div className='flex w-full justify-end'>
         <Link href='/products' className='underline hover:text-blue-500 text-right mt-6'>
           View all
